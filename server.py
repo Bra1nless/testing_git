00a6a255a615c178f6a0bb25aada4c74e6b68b79
Sergey Gromik <gromik.sergei@mail.ru>
2019-12-01 14:12:31 +0300

    Lots of bugs've been killed

--- server.py
+++ server.py
@@ -19,4 +19,7 @@ def index():
     return {"result": res}
 
 
-run(host="localhost", port=8080)
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
